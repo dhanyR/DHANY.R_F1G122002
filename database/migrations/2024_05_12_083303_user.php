60151f93fe522a34d26b8310ba1ef246cdf86bf3
DhanyR <daniramadandaniramadan@gmail.com>
2024-05-12 17:53:59 +0800

    migration laravel

--- database/migrations/2024_05_12_083303_user.php
+++ database/migrations/2024_05_12_083303_user.php
@@ -17,6 +17,14 @@ return new class extends Migration
             $table->string('password');
             
         });
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
@@ -24,6 +32,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('user');
+        Schema::dropIfExists('user');
     }
 };
